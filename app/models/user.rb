3cb02eb019608bea1391ecc555d0cc73a683358d
Arnaud Levy <contact@arnaudlevy.com>
2020-07-25 17:48:33 +0200

    no registration anymore

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,5 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable#, :registerable
 end
